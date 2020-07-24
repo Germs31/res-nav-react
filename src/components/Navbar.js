ef3582014a0fe92e791b990ee38ef7a76d6df680
German Nunez <german90nunez@gmail.com>
2020-07-24 15:19:34 -0700

    animation and states set for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/imgs/angryFace.png'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [status, isStatus] = useState('open')
     return (
         <nav>
             <div className="logo">
@@ -15,6 +17,11 @@ const Navbar = () => {
                 <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
                 <NavLink className="nav-link" to="/contact">Contact</NavLink>
             </ul>
+            <div className="hamburger-container" onClick={() => setIsOpen(!isOpen)}>
+                <i className={status}></i>
+                <i className={status}></i>
+                <i className={status}></i>
+            </div>
         </nav>
     )
 }
