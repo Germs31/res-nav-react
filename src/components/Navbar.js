bd018ac224ebea9604f0ca3f3b185e73902af45b
German Nunez <german90nunez@gmail.com>
2020-07-24 16:17:41 -0700

    slide menu for mobile complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,37 @@ import logo from '../assets/imgs/angryFace.png'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
-    const [status, isStatus] = useState('open')
     return (
-        <nav>
-            <div className="logo">
-                <img src={logo} />
+        <div className="nav-container">
+            <nav>
+                <div className="logo">
+                    <img src={logo}  alt="logo"/>
+                </div>
+                <ul>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/about">About</NavLink>
+                    <NavLink className="nav-link" to="/blog">Blog</NavLink>
+                    <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
+                    <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                </ul>
+
+                <div className="hamburger-container" onClick={() => setIsOpen(!isOpen)}>
+                    <i className={isOpen ? "open" : ""}></i>
+                    <i className={isOpen ? "open" : ""}></i>
+                    <i className={isOpen ? "open" : ""}></i>
+                </div>
+            </nav>
+
+            <div className={isOpen ? "res-nav show" : "res-nav"} onClick={() => setIsOpen(!isOpen)}>
+                <ul>
+                    <NavLink className="res-link" to="/">Home</NavLink>
+                    <NavLink className="res-link" to="/about">About</NavLink>
+                    <NavLink className="res-link" to="/blog">Blog</NavLink>
+                    <NavLink className="res-link" to="/portfolio">Portfolio</NavLink>
+                    <NavLink className="res-link" to="/contact">Contact</NavLink>
+                </ul>
             </div>
-            <ul>
-                <NavLink className="nav-link" to="/">Home</NavLink>
-                <NavLink className="nav-link" to="/about">About</NavLink>
-                <NavLink className="nav-link" to="/blog">Blog</NavLink>
-                <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
-                <NavLink className="nav-link" to="/contact">Contact</NavLink>
-            </ul>
-            <div className="hamburger-container" onClick={() => setIsOpen(!isOpen)}>
-                <i className={status}></i>
-                <i className={status}></i>
-                <i className={status}></i>
-            </div>
-        </nav>
+        </div>
     )
 }
 
