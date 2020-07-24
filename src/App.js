d774afc08ae6282c001d760171e84c9f49c8325c
German Nunez <german90nunez@gmail.com>
2020-07-24 12:38:21 -0700

    pages and react router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+import Portfolio from './pages/Portfolio';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/" render={() => <Home/> } />
+        <Route path="/about" render={() => <About/> } />
+        <Route path="/contact" render={() => <Contact/> } />
+        <Route path="/blog" render={() => <Blog/> } />
+        <Route path="/portfolio" render={() => <Portfolio/> } />
+      </Switch>
     </div>
   );
 }
